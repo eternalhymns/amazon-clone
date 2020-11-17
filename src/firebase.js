959634b97dc9b0e5db38cbb8aab70c54b3499dc4
Seoyeon <eternalhymns@gmail.com>
2020-11-17 12:06:45 +0900

    헤더 완료

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAAS30DsbRDH9ttuojN0d-jr13pJXjUlt8",
+  authDomain: "challenge-8f15d.firebaseapp.com",
+  databaseURL: "https://challenge-8f15d.firebaseio.com",
+  projectId: "challenge-8f15d",
+  storageBucket: "challenge-8f15d.appspot.com",
+  messagingSenderId: "316885994068",
+  appId: "1:316885994068:web:8d6283bae18403823ebbc1",
+  measurementId: "G-V5RGP54SCH"
+};
