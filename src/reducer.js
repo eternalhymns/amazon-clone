ebd02f167215e9066e2827b1e31970083c8ab0e5
Seoyeon <eternalhymns@gmail.com>
2020-11-18 07:24:42 +0900

    상품 장바구니에 담을 때마다 장바구니 옆의 숫자 1씩 늘어남

---
+++ src/reducer.js
@@ -0,0 +1,45 @@
+// export const initalState = {
+//   basket: [],
+// };
+
+// const reducer = (state = { item: [] }, action) => {
+//   console.log(action);
+//   switch (action.type) {
+//     case "ADD_TO_BASKET":
+//       return {
+//         ...state,
+//         basket: [...state.basket, action.item],
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+// export default reducer;
+
+
+export const initialState = {
+  basket: [],
+  user: null
+};
+
+// Selector
+export const getBasketTotal = (basket) => 
+  basket?.reduce((amount, item) => item.price + amount, 0);
+
+const reducer = (state, action) => {
+  console.log(action);
+  switch (action.type) {
+    case "ADD_TO_BASKET":
+      return {
+        ...state,
+        basket: [...state.basket, action.item],
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
+
+
